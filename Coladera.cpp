dbe68747007cff40294cff177fab5a92e64a7836
Josuec1389 <54084002+Josuec1389@users.noreply.github.com>
2019-10-01 17:40:29 -0500

    Version final Coladera
    Esta es la versión final del método por Eratostenes

--- Coladera.cpp
+++ Coladera.cpp
@@ -1,2 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void llenarunos(int vec[],int n);
+void mostrar(int vec[], int n);
+void coladera(int vec[],int n);
+
+int main() {
+	
+	int vec[1000];
+	llenarunos(vec,1000);
+	coladera(vec,1000);
+	mostrar(vec,1000);
+			
+	return 0;
+}
+
+void llenarunos(int vec[],int n){
+	for(int i=0;i<n;i++)
+	{
+		vec[i]=1;
+	}
+}
+
+void coladera(int vec[],int n)
+{
+	for(int i=2;i<n;i++){
+		if (vec[i]) //vec==1
+		for(int j=2;i*j<n;j++){
+			vec[i*j]=0;
+		}
+	}
+}
+
+void mostrar(int vec[], int n)
+{
+	for(int i=2;i<n;i++)
+	{
+		if(vec[i]){
+			cout<<i<<" ";
+		}
+	}
+}
